9b2a7195605e18ef02a605af8431e985c510636c
Jonathan Grimaldo <jgrima85@gmail.com>
2021-02-01 17:25:46 -0600

    Fin seccion 3 Ruteo y hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PortafolioComponent },//Cuando my url este vacio va a redirigir a mi Home
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }, //Excepcion, si no navega a las opciones que redirija a HOME
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], //Declarando rutas de raiz .forRoot ((.forChild))
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
