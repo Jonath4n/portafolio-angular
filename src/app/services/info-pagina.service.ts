3f815bb1de5a2a94157de5728b945323c33a1b1c
Jonathan Grimaldo <jgrima85@gmail.com>
2021-02-03 16:51:58 -0600

    Fin Seccion 5 Firebase Data

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,32 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
-
+  equipo: any[]= [];
   constructor( private http: HttpClient ) {
 
     //console.log('servicio de info pagina listo');
-
+    this.cargarInfo();
+    this.cargarEquipo();
     //Leer archivo JSON
+  }
+
+
+  private cargarInfo(){
+
     this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) => {
 
       this.cargada = true;
       this.info = resp;
       console.log( resp );
 
+    });
+  }
+
+  private cargarEquipo(){
+
+    this.http.get('https://angular-htmls-default-rtdb.firebaseio.com/equipo.json').subscribe( (resp: any) => {
+      this.equipo = resp;
+      console.log(resp);
     });
 
   }
